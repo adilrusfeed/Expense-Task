14b82756609f2c165e722a5d9d9c9a185e75a700
adilrusfeed <adilrusfeed@gmail.com>
2024-09-24 23:21:29 +0530

    hive initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:expensetracker/model/expense_model.dart';
 import 'package:expensetracker/view/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(ExpenseModelAdapter().typeId)) {
+    Hive.registerAdapter(ExpenseModelAdapter());
+  }
+  await Hive.openBox<ExpenseModel>('expense_box');
   runApp(const MyApp());
 }
 
@@ -12,13 +20,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Homepage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Homepage());
   }
 }
